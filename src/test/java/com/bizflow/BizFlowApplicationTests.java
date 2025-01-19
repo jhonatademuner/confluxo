a5c8a01d274895c1dcc9fad712c11cfd1489b300
jhonatademuner <jhonata.demuner@gmail.com>
2025-01-19 17:58:51 -0300

    build: initial setup

---
+++ src/test/java/com/bizflow/BizFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
